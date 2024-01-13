af72ce6328a74147cd2e8eea851af5a8d4f0e9ce
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-01-13 19:22:02 +0530

    locomotie tried with the combination of scrolltrigger

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-// gsap.from("#box",{
-//       x:800,
-//       duration:2,
-//       delay:1,
-//       opacity:0,
-// });
-// gsap.from("#page1",{
-//       backgroundColor:"pink",
-//       delay:1,
-//       duration:2,
-//       })
+gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
 gsap.from("#page2",{
       backgroundColor:"red",
       // duration:5,
       scrollTrigger:{
+            scroller:"#main",
       trigger:"#page2",
       start : "top 50%",
       markers :true,
@@ -23,6 +43,7 @@ gsap.from("#page2",{
 gsap.to("#page2 h1",{
       transform : "translateX(-85%)",
       scrollTrigger:{
+            scroller:"#main",
             trigger:"#page2",
            scrub: 2,
            markers :true,
