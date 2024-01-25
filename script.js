cdf22949dc485d87fb827a64ea369a9f67975884
Atul-raj07 <atulrajgupta12j@gmail.com>
2024-01-25 23:52:44 +0530

    new animation learned

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  centeredSlides: false,
-  slidesPerGroupSkip: 1,
-  grabCursor: true,
-  keyboard: {
-    enabled: true,
-  },
-  breakpoints: {
-    769: {
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-    },
-  },
-  scrollbar: {
-    el: ".swiper-scrollbar",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+var bg = document.querySelector("#bg")
+
+
+
+var tl = gsap.timeline()
+
+
+var one = document.querySelector("#one")
+var onetxt = one.innerHTML;
+ var splitted = onetxt.split("");
+
+//  console.log(splitted)
+ var clutter = "";
+splitted.forEach(function(e) {
+// console.log(e)
+    
+    clutter += `<span>${e}</span>`
+} 
+);
+one.innerHTML = clutter
+console.log(clutter)
+
+// 2nd
+var two = document.querySelector("#two")
+var twotxt = two.innerHTML;
+ var splitted2 = twotxt.split("");
+
+//  console.log(splitted)
+ var clutter2 = "";
+splitted2.forEach(function(e) {
+// console.log(e)
+    
+    clutter2 += `<span>${e}</span>`
+} 
+);
+two.innerHTML = clutter2
+console.log(two)
+
+
+bg.addEventListener("mouseenter",function(){ 
+ tl.to("span",{
+    y:"-100%",
+    stagger:0.05
+ })
+})
+bg.addEventListener("mouseleave",function(){ 
+ tl.to("span",{
+    y:0,
+    stagger:0.05,
+ })
+})
